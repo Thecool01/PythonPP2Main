168b2244eae54b66ee6ac38bb16b477b16d3bbcc
Nikolay Ishutin <n_ishutin@kbtu.kz>
2025-02-15 13:48:34 +0500

    lab4

---
+++ Python/lab4/date.py
@@ -0,0 +1,8 @@
+import datetime
+
+
+current_date = datetime.datetime.today()
+datetest = datetime.date(2023, 2, 13)
+## int_current_date = current_date.strftime("")
+## new_date = 
+print(print(datetest))
