dd806f03e6b6923b6d46ca47e7015aaea67b050a
Nikolay1 <2097nikola2097@gmail.com>
2025-02-15 21:20:26 +0500

    lab 4

--- Python/lab4/date.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-
-current_date = datetime.datetime.today()
-datetest = datetime.date(2023, 2, 13)
-## int_current_date = current_date.strftime("")
-## new_date = 
-print(print(datetest))
